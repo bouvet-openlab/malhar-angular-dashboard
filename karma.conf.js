9fe1cfe83f60a6a2d1cff383407698049c8594da
unknown <EIBEL@LT-757898.statoil.net>
2014-10-24 08:13:23 +0200

    Upgraded karma-coverage and added generic + browser based coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,71 +1,80 @@
 // Karma configuration
 // http://karma-runner.github.io/0.10/config/configuration-file.html
 
-module.exports = function(config) {
-  config.set({
+module.exports = function (config) {
+    config.set({
 
-    preprocessors: {
-      // which files to show in coverage report
-      'src/**/*.js': ['coverage']
-    },
+        preprocessors: {
+            // which files to show in coverage report
+            'src/**/*.js': ['coverage']
+        },
 
-    reporters: ['dots', 'coverage'],
+        reporters: ['dots', 'coverage'],
 
-    coverageReporter: {
-      type: 'html',
-      dir: 'coverage/'
-    },
+        coverageReporter: {
+            reporters: [
+                { type: 'html', dir: 'coverage/', subdir: '.' },
+                //{ type: 'teamcity' },
+                {
+                    type: 'html', dir: 'coverage/browsers/', subdir: function (browser) {
+                    // normalization process to keep a consistent browser name accross different
+                    // OS
+                    return browser.toLowerCase().split(/[ /-]/)[0];
+                }
+                }
+            ]
+        },
 
-    // base path, that will be used to resolve files and exclude
-    basePath: '',
+        // base path, that will be used to resolve files and exclude
+        basePath: '',
 
-    // testing framework to use (jasmine/mocha/qunit/...)
-    frameworks: ['jasmine'],
+        // testing framework to use (jasmine/mocha/qunit/...)
+        frameworks: ['jasmine'],
 
-    // list of files / patterns to load in the browser
-    files: [
-      'bower_components/jquery/jquery.js',
-      'bower_components/lodash/dist/lodash.js',
-      'bower_components/angular/angular.js',
-      'bower_components/angular-mocks/angular-mocks.js',
-      'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
-      'src/directives/dashboard.js',
-      'src/directives/*.js',
-      'src/models/*.js',
-      'src/controllers/*.js',
-      'template/*.js',
-      'test/mock/**/*.js',
-      'test/spec/**/*.js'
-    ],
+        // list of files / patterns to load in the browser
+        files: [
+            'bower_components/jquery/jquery.js',
+            'bower_components/lodash/dist/lodash.js',
+            'bower_components/angular/angular.js',
+            'bower_components/angular-mocks/angular-mocks.js',
+            'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+            'src/directives/dashboard.js',
+            'src/directives/*.js',
+            'src/models/*.js',
+            'src/controllers/*.js',
+            'template/*.js',
+            'test/mock/**/*.js',
+            'test/spec/**/*.js'
+        ],
 
-    // list of files / patterns to exclude
-    exclude: [],
+        // list of files / patterns to exclude
+        exclude: [],
 
-    // web server port
-    port: 8080,
+        // web server port
+        port: 8080,
 
-    // level of logging
-    // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
-
-
-    // Start these browsers, currently available:
-    // - Chrome
-    // - ChromeCanary
-    // - Firefox
-    // - Opera
-    // - Safari (only Mac)
-    // - PhantomJS
-    // - IE (only Windows)
-    browsers: ['PhantomJS'],
+        // level of logging
+        // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+        logLevel: config.LOG_INFO,
 
 
-    // Continuous Integration mode
-    // if true, it capture browsers, run tests and exit
-    singleRun: false
-  });
+        // enable / disable watching file and executing tests whenever any file changes
+        autoWatch: true,
+
+
+        // Start these browsers, currently available:
+        // - Chrome
+        // - ChromeCanary
+        // - Firefox
+        // - Opera
+        // - Safari (only Mac)
+        // - PhantomJS
+        // - IE (only Windows)
+        browsers: ['PhantomJS'],
+
+
+        // Continuous Integration mode
+        // if true, it capture browsers, run tests and exit
+        singleRun: false
+    });
 };
